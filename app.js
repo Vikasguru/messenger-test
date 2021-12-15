0e4b29ef0e21cdbacd2ad81b78ba72197b46295a
Vikasguru <vikivikas41@gmail.com>
2021-12-15 13:41:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -72,37 +72,39 @@ app.get('/webhook', (req, res) => {
 // Creates the endpoint for your webhook
 app.post('/webhook', (req, res) => {
     let body = req.body;
+    console.log("body",body);
 
     // Checks if this is an event from a page subscription
-    if (body.object === 'page') {
+//     if (body.object === 'page') {
 
-        // Iterates over each entry - there may be multiple if batched
-        body.entry.forEach(function (entry) {
+//         // Iterates over each entry - there may be multiple if batched
+//         body.entry.forEach(function (entry) {
 
-            // Gets the body of the webhook event
-            let webhookEvent = entry.messaging[0];
-            console.log(webhookEvent);
+//             // Gets the body of the webhook event
+//             let webhookEvent = entry.messaging[0];
+//             console.log(webhookEvent);
 
-            // Get the sender PSID
-            let senderPsid = webhookEvent.sender.id;
-            console.log('Sender PSID: ' + senderPsid);
+//             // Get the sender PSID
+//             let senderPsid = webhookEvent.sender.id;
+//             console.log('Sender PSID: ' + senderPsid);
 
-            // Check if the event is a message or postback and
-            // pass the event to the appropriate handler function
-            if (webhookEvent.message) {
-                handleMessage(senderPsid, webhookEvent.message);
-            } else if (webhookEvent.postback) {
-                handlePostback(senderPsid, webhookEvent.postback);
-            }
-        });
+//             // Check if the event is a message or postback and
+//             // pass the event to the appropriate handler function
+//             if (webhookEvent.message) {
+//                 handleMessage(senderPsid, webhookEvent.message);
+//             } else if (webhookEvent.postback) {
+//                 handlePostback(senderPsid, webhookEvent.postback);
+//             }
+//         });
 
-        // Returns a '200 OK' response to all requests
-        res.status(200).send('EVENT_RECEIVED');
-    } else {
+//         // Returns a '200 OK' response to all requests
+//         res.status(200).send('EVENT_RECEIVED');
+//     } else {
 
-        // Returns a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
-    }
+//         // Returns a '404 Not Found' if event is not from a page subscription
+//         res.sendStatus(404);
+//     }
+     res.status(200).send('EVENT_RECEIVED');
 });
 
 // Handles messages events
